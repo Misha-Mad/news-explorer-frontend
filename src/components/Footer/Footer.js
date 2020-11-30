d4bfac2999c5410995bd1421cb4ea49a7d721329
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-11-30 13:33:54 +0300

    fix footer with eslint error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,13 @@ function Footer() {
 		<footer className="footer">
 			<p className="footer__copyright">© 2020 Supersite, Powered by News API</p>
 			<Link to="/" className="footer__link">Главная</Link>
-			<a target="_blank" href="https://praktikum.yandex.ru/" className="footer__link">Яндекс.Практикум</a>
-			<a target="_blank" href="https://github.com/Misha-Mad" className="footer__github"></a>
-			<a target="_blank" href="https://vk.com/m_3axapov" className="footer__facebook"></a>
+			<a target="_blank" rel="noreferrer" href="https://praktikum.yandex.ru/" className="footer__link">Яндекс.Практикум</a>
+			<a target="_blank" rel="noreferrer" href="https://github.com/Misha-Mad" className="footer__github">
+			    текст для eslint
+			</a>
+			<a target="_blank" rel="noreferrer" href="https://vk.com/m_3axapov" className="footer__facebook">
+			    текст для eslint
+			</a>
 		</footer>
 	)
 }
